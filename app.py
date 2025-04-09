e2b5825120dd58b9c3b661c5818bf0716c576a11
sandeepnmenon8888 <sandeepnmenon88.88@gmail.com>
2025-04-09 20:58:49 +0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello, World! Deployed via Jenkins & OpenShift with app source files and Dockerfile in GitRep 🚀</h1>"
+    return "<h1>Hello, World! Deployed via Jenkins & OpenShift with app source files and Dockerfile in GitRepo 🚀</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
