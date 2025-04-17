a0b4cda0b52497ad003765cfd7f2a3b7a21e77f6
sandeepnmenon8888 <sandeepnmenon88.88@gmail.com>
2025-04-17 17:05:30 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello, World! Deployed via Jenkins & OpenShift with app source files and Dockerfile in GitRepo 🚀</h1>"
+    return "Hello World"
+
+@app.route('/read_file')
+def read_file():
+    try:
+        with open('/data/testfile.txt', 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return "File not found."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
